0f1d38aed8484a63f04ad66e52a6eda7bb35bfa2
DESKTOP-12JO8K5\Juanma <jsangam2606@iescarrillo.es>
2026-05-24 22:44:28 +0200

    Arreglado musica

--- app/src/main/java/com/example/proyectofinal/activities/ContenedorActivity.java
+++ app/src/main/java/com/example/proyectofinal/activities/ContenedorActivity.java
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.proyectofinal.MusicService;
 import com.example.proyectofinal.R;
 import com.example.proyectofinal.fragments.AjustesFragment;
 import com.example.proyectofinal.fragments.AmenazaFragment;
@@ -16,19 +15,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class ContenedorActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNav;
-    private boolean musicEnabled = true ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contenedor);
-
-        //Iniciar música SOLO si está activada (ej: desde SharedPreferences)
-        if (musicEnabled) {
-            Intent musicIntent = new Intent(this, MusicService.class);
-            startService(musicIntent);
-            MusicService.startMusic(this, R.raw.background_music);
-        }
 
         bottomNav = findViewById(R.id.bottomNav);
 
@@ -44,14 +35,13 @@ public class ContenedorActivity extends AppCompatActivity {
                 fragment = new HomeFragment();
             } else if (id == R.id.nav_plantas) {
                 fragment = new PlantaFragment();
-
             } else if (id == R.id.nav_chat) {
                 Intent i = new Intent(this, ChatActivity.class);
                 startActivity(i);
                 return true;
-            }else if (id == R.id.nav_amenazas) {
+            } else if (id == R.id.nav_amenazas) {
                 fragment = new AmenazaFragment();
-            }else if (id == R.id.nav_ajustes) {
+            } else if (id == R.id.nav_ajustes) {
                 fragment = new AjustesFragment();
             }
 
@@ -68,34 +58,5 @@ public class ContenedorActivity extends AppCompatActivity {
             return true;
         }
         return false;
-    }
-
-    //Detener música al cerrar la app
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (musicEnabled) {
-            Intent musicIntent = new Intent(this, MusicService.class);
-            stopService(musicIntent);
-            MusicService.stopMusic();
-        }
-    }
-
-    // Pausar música si la app va a segundo plano
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (musicEnabled) {
-            MusicService.pauseMusic();
-        }
-    }
-
-    //Reanudar música al volver a la app
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (musicEnabled) {
-            MusicService.resumeMusic();
-        }
     }
 }
